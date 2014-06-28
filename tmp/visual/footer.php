3b62ed6f5d60e7e3c1cafac8ac4f9999bbfae840
radhagovinda <brainscape@live.com>
2014-06-28 16:09:36 +0530

    Added header and footer
    Added header.php and footer.php

---
+++ tmp/visual/footer.php
@@ -0,0 +1,22 @@
+<STYLE type=text/css>
+<!--
+body {
+	margin-left: 0px;
+	margin-top: 0px;
+	margin-right: 0px;
+	margin-bottom: 0px;
+}
+.style1 {
+	color: #FFFFFF;
+	font-family: Arial, Helvetica, sans-serif;
+	font-weight: bold;
+	font-size: 12px;
+}
+.style2 {font-family: Arial, Helvetica, sans-serif; font-size: 12px; color: #FFFFFF;}
+-->
+</STYLE>
+<TABLE cellSpacing=0 cellPadding=10 width="100%" border=0>
+  <TBODY>
+  <TR>
+    <TD height=80 background=images/bg.png><SPAN class=style1>© </SPAN><SPAN 
+      class=style2>ShikshaSutram</SPAN></TD></TR></TBODY></TABLE>
